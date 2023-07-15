26e2454303a34e2414515a998a8317c2b4c45426
Marco Torres <marcotv505@gmail.com>
2023-07-15 01:21:18 -0700

    metodo get

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI(
-    title='App con FastAPI',
-    version='0.0.1'
-)
+app = FastAPI(title="App con FastAPI", version="0.0.1")
+
+video_games = [
+    {
+        "id": 1,
+        "name": "Disgaea",
+        "description": "Tactical JRPG",
+        "realease_year": 2003,
+        "developer": "Nippon Ichi Software",
+    }
+]
 
 
-@app.get('/', tags=['home'])
+@app.get("/", tags=["home"])
 def main():
-    return 'Hello World'
+    return HTMLResponse("<h1>Hello World</h1>")
+
+
+@app.get("/games", tags=["games"])
+def get_games():
+    return video_games
