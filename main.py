dc42053d5a1ad440fafdf1f5886a09d5b952561e
Marco Torres <marcotv505@gmail.com>
2023-07-15 01:53:16 -0700

    metodo post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI(title="App con FastAPI", version="0.0.1")
@@ -44,3 +44,23 @@ def get_game(id: int):
 @app.get("/games/", tags=["games"])
 def get_game_by_genre(genre: str):
     return [game for game in video_games if game["genre"] == genre]
+
+
+@app.post("/games", tags=["games"])
+def create_game(
+    id: int = Body(),
+    name: str = Body(),
+    description: str = Body(),
+    realease_year: int = Body(),
+    developer: str = Body(),
+    genre: str = Body(),
+):
+    game = {
+        "id": id,
+        "name": name,
+        "description": description,
+        "realease_year": realease_year,
+        "developer": developer,
+        "genre": genre,
+    }
+    video_games.append(game)
