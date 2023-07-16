64caa20dcbd5ccfaf7183212a1ca44514b88cbad
Marco Torres <marcotv505@gmail.com>
2023-07-16 08:33:21 -0700

    readme y cambio en credenciales

---
+++ README.md
@@ -0,0 +1,25 @@
+# Video Games API
+
+API made with `Python`, `SQLAlchemy` and `FastAPI` for video games.
+
+## Setup
+
+* Rename file `example.env` to `.env` and put your data.
+
+## Install
+
+**It is recommended to create a virtual environment**
+
+```bash
+pip install -r requirements.txt
+```
+
+## Run App
+
+```bash
+uvicorn main:app --port 8000t
+```
+
+## Documentation
+
+[Click here](http://127.0.0.1:8000/docs)
--- routers/auth.py
+++ routers/auth.py
@@ -1,14 +1,19 @@
+import os
+
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
-from utils.jwt_manager import create_token
 from schemas.auth import User
+from utils.jwt_manager import create_token
 
 auth_router = APIRouter()
 
 
 @auth_router.post("/login", tags=["auth"])
 def login(user: User):
-    if user.email == "admin@email.com" and user.password == "admin":
+    if (
+        user.email == os.environ["ADMIN_EMAIL"]
+        and user.password == os.environ["ADMIN_PASSWORD"]
+    ):
         token: str = create_token(user.model_dump())
         return JSONResponse(content=token, status_code=200)
